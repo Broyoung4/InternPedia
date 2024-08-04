2913b1f92d0a6a99b3ee448e301407a0327a15c8
Adebayo Nurudeen <123829629+Broyoung4@users.noreply.github.com>
2024-08-04 12:05:38 +0000

    first commit

---
+++ src/Tasks.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ACTIONS, Task } from './App.tsx'; // Import ACTIONS and Task from App.tsx
+
+export default function Tasks({ tasks, dispatch }: { tasks: Task; dispatch: React.Dispatch<any> }) {
+  return (
+    <div className='app_tasks'>
+      <input 
+        type='checkbox' 
+        onClick={() => dispatch({ type: ACTIONS.TOGGLE_TASK, payload: { id: tasks.id } })}  
+      />
+      <h3 className={`${tasks.completed === true ? 'text-cross' : ''}`} >{tasks.name}</h3>
+      <button onClick={() => dispatch({type: ACTIONS.DELETE_TASK, payload: { id: tasks.id } })}>DELETE</button>
+    </div>
+  )
+}
