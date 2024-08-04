3d82e8aa42c389ff5777511f0bd8ef9bb1cf4567
Adebayo Nurudeen <123829629+Broyoung4@users.noreply.github.com>
2024-08-04 22:31:58 +0000

    secondTask

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+export default function App() {
+   return (
+    <main className='h-screen flex items-center justify-center'>
+      <div className='bg-white p-4 rounded-lg shadow-lg'>
+      
+      </div>
+      <h1 className="text-3xl font-bold underline">
+        Weather App 💨
+      </h1>
+    </main>
+  )
+}
