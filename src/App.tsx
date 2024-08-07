6d2135caf582aa31953c92bcba9eefdced2f61a3
Adebayo Nurudeen <123829629+Broyoung4@users.noreply.github.com>
2024-08-07 06:55:28 +0000

    updating Task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import React, { useState, useEffect } from 'react'
 import { TiWeatherCloudy,TiWeatherDownpour, TiWeatherSunny, TiWeatherNight, TiWeatherShower,TiWeatherSnow, TiWeatherStormy, TiWeatherPartlySunny  } from "react-icons/ti";
-
 import { WiHumidity } from "react-icons/wi";
 import { FaWind } from "react-icons/fa";
 
@@ -16,17 +15,22 @@ export default function App() {
   const [icon, setIcon] = useState(null);
   const [error, setError] = useState(false);
   const [date, setDate] = useState('');
-
+  const [dataObj, setDataObj] = useState([]);
+  
   //handle onchange events
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCityInput(e.target.value);
   }
 
-  const handleSubmit = (e) => {
+  //handle submit event
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    //fetching data from api
-    const apiKey = 'acaaa3475d48d47200cf871d36a5f380';
-		    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    //FETCHING DATA FROM API
+
+    //api variables
+    const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+        //weather fetch function
         const weatherFetch = async (city: string) => {
           const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
           const data = await response.json();
@@ -61,12 +65,13 @@ export default function App() {
           setIcon(iconElement); // Pass the icon element to setIcon
           
         }
-        weatherFetch(cityInput);
-    //setCity(cityInput);
-    setCityInput('')
+        
         }
+    weatherFetch(cityInput);
+    setCityInput('')
   }
-  
+
+  //useEffect for Date and time
   useEffect(() => {
            const intervalId = setInterval(() => {
                setDate(new Date().toLocaleString());
@@ -74,25 +79,26 @@ export default function App() {
              // Cleanup: Clear the interval when the component unmounts
              return () => clearInterval(intervalId);
            }, []); 
- 
+
+  
    return (
     <main className='h-screen flex flex-col items-center justify-center'>
       <h1 className='sm:text-5xl text-4xl font-black capitalize'>Weather App</h1>
       <div className='w-[400px] flex flex-col items-center justify-center p-10 shadow-2xl mt-10 bg-white rounded-xl'>
-        <form className='' onSubmit={handleSubmit}>
+        <form action='' onSubmit={handleSubmit}>
           <div className='flex justify-between items-center gap-4 mb-10'>
             <input type='text' value={cityInput} placeholder='Enter City Name' className='px-4 py-2 w-[80%]' onChange={handleChange}/>
             <button className='outline-0 px-4 py-2 bg-black text-white rounded-xl hover:text-black hover:bg-white border-2 hover:border-black' type='submit'>Search</button>
           </div>
-          <div className='flex justify-center items-center gap-4'>
-            
-            {icon}
-          </div>
+          
           {error ? <p className='text-red-500'>City Not Found</p>
           : (
             <div>
+              <div className='flex justify-center items-center gap-4'>
+                {icon}
+              </div>
               <div className='flex flex-col justify-center items-center gap-6 mb-10'>
-                <h1 className='text-4xl font-black text-red-500'>{city==='' ? 'Unsearched' : city}</h1>
+                <h1 className='text-4xl font-black text-red-500'>{city==='' ? 'Waiting for input..' : city}</h1>
                 <p className='text-xl'>{date}</p>
                 <p className='text-3xl font-bold'>{temp === 0 ? '--': temp}°C</p>
               </div>
